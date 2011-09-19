b0001af8ac14dec369929037be89954948e8be68
Everton Fraga <everton.frag@gmail.com>
2011-09-19 02:11:05 -0300

    Drawing object with support for texts only. Now accepts position with css-like expressions (eg: 'center', 'bottom right')

--- Client/Drawing.js
+++ Client/Drawing.js
@@ -14,13 +14,21 @@ Redread.Drawing = {
         }
     },
     text: function(text, options){
+        // console.log(text, options);
+        // If text is a function, executes it and grabs its result
+        if (typeof text == 'function') {
+            text = text();
+        };
+        
         options = options || {};
         options = {
-            font: options.font || '50pt Impact',
+            font: options.font || '20pt sans-serif',
             x: options.x || '0',
-            y: options.y || '0',
+            y: options.y || '20',
             color: options.color || '#eee',
-            stroke: options.stroke || '1'
+            stroke: options.stroke || '1',
+            position: options.position || '',
+            textBaseline: options.textBaseline || 'alphabetic'
         };
         
         var ctx = Redread.drawContext;
@@ -28,13 +36,20 @@ Redread.Drawing = {
             fillStyle: ctx.fillStyle,
             strokeStyle: ctx.strokeStyle,
             lineWidth: ctx.lineWidth,
-            font: ctx.font
+            font: ctx.font,
+            textBaseline: ctx.textBaseline
         };
-        console.log("measure text: ", ctx.measureText(text));
 
         ctx.fillStyle = options.color;
         ctx.font = options.font;
-        console.log("ctx: ", ctx);
+        // Ensures that lowercase p or q doesn't stay out of the box
+        ctx.textBaseline = options.textBaseline;
+        
+        if (options.position) {
+            var coords = this.parsePosition(text, options);
+            options.x = coords.x;
+            options.y = coords.y;
+        };
 
         ctx.fillText(text, options.x, options.y);
         if (options.stroke > 0) {
@@ -44,6 +59,65 @@ Redread.Drawing = {
         
         // Returning old values to context
         this.oldContext2Context(oldctx, ctx);
+    },
+    
+    /**
+     * Parses position just like in CSS. Only words allowed for now
+     * Examples: 'top left', 'center', 'right bottom'
+     */
+    parsePosition: function(text, options){
+        // Regex for matching position values
+        var regexSingle = /^top|bottom|left|right|center$/,
+            regexDouble = /^(top|bottom|center) (left|right|center)$/;
+
+        if (regexSingle.test(options.pos) && regexDouble.test(options.pos)){
+            throw new Error('Bad position "' + pos + '"');
+        }
+
+        var 
+            ctx = Redread.drawContext,
+            pos = options.position,
+            words = pos.split(' '),
+            
+            txtWidth = ctx.measureText(text).width,
+            txtHeight = parseInt(options.font, 10),
+            
+            canvasWidth = ctx.canvas.width,
+            canvasHeight = ctx.canvas.height,
+            
+            x = 0,
+            y = txtHeight;
+            
+        for(var i in words){
+            switch(words[i]){
+                case 'top':
+                    y = txtHeight;
+                    break;
+                case 'bottom':
+                    y = canvasHeight;
+                    break;
+                case 'left':
+                    x = 0;
+                    break;
+                case 'right':
+                    x = canvasWidth - txtWidth;
+                    break;
+                case 'center':
+                    if(words.length === 1){
+                        x = canvasWidth/2 - txtWidth/2;
+                        y = canvasHeight/2 + txtHeight/2;
+                    }
+                    else if(i === 0) {
+                        y = canvasHeight/2 - txtHeight/2;
+                    }
+                    else if(i === 1){
+                        x = canvasWidth/2 - txtWidth/2;
+                    }
+                    break;
+            }
+        }
+        
+        return {x: x, y: y};
     }
 };
 
