aaf77369578c5a42839de43eb5f6149febe85098
Everton Fraga <everton.frag@gmail.com>
2011-09-24 13:03:15 -0300

    Utils class

---
+++ Shared/Utils.js
@@ -0,0 +1,15 @@
+/**
+ * Utils
+ * 
+ */
+Redread.Utils = {
+    select: function(obj, values){
+        var result = values['default'] || null;
+        for(v in values){
+            if (v == obj) {
+                result = values[v];
+            };
+        }
+        return result;
+    }
+};
