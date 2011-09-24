c5eb3fbbef9fba65281e79477b03f91e085b715f
Everton Fraga <everton.frag@gmail.com>
2011-09-24 18:42:41 -0300

    minor changes

--- Demo/Input/booServer.js
+++ Demo/Input/booServer.js
@@ -3,11 +3,11 @@ var server = require('../../Server/Server.js').Redread.server();
 
 var boo = Redread.gameObject(10, 10, 'boo', true);
 
-boo.registerKeyEvent('up', function() { this.posY -= 10; });
-boo.registerKeyEvent('down', function() { this.posY += 10; });
-boo.registerKeyEvent('left', function() { this.posX -= 10; });
-boo.registerKeyEvent('right', function() { this.posX += 10; });
-boo.registerKeyEvent('space', function() { this.aberto = !this.aberto; });
+boo.registerEvent('up', function() { this.posY -= 10; });
+boo.registerEvent('down', function() { this.posY += 10; });
+boo.registerEvent('left', function() { this.posX -= 10; });
+boo.registerEvent('right', function() { this.posX += 10; });
+boo.registerEvent('space', function() { this.aberto = !this.aberto; });
 
 server.addObjects(boo);
 server.addConfiguration({players: 1});
