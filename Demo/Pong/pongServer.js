10bb66179adbe8ef21371406e74572d3256efbb5
Everton Fraga <everton.fraga@corp.globo.com>
2011-09-24 20:41:00 -0300

    Bug that was introduced in pong

--- Demo/Pong/pongServer.js
+++ Demo/Pong/pongServer.js
@@ -1,12 +1,15 @@
 var Redread = require('../../Server/GameObject.js').Redread;
 var server = require('../../Server/Server.js').Redread.server();
 
-var leftPad = Redread.gameObject(10, 10, 'lefty');
-var rightPad = Redread.gameObject(470, 10, 'righty');
+var leftPad = Redread.gameObject(10, 10, 'lefty', true);
+var rightPad = Redread.gameObject(470, 10, 'righty', true);
 
 leftPad.registerEvent('up', function() { this.posY -= 10; });
 leftPad.registerEvent('down', function() { this.posY += 10; });
 rightPad.registerEvent('up', function() { this.posY -= 10; });
 rightPad.registerEvent('down', function() { this.posY += 10; });
 server.addObjects(leftPad, rightPad);
+server.addConfiguration({
+    "players": 2
+});
 server.init();
