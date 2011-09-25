19479cd4258d97056aad491fd1915c3ae3bc486b
Everton Fraga <everton.frag@gmail.com>
2011-09-25 19:53:36 -0300

    Fixing demos

--- Demo/Input/booServer.js
+++ Demo/Input/booServer.js
@@ -3,13 +3,37 @@ var server = require('../../Server/Server.js').Redread.server();
 
 var boo = Redread.gameObject(10, 10, 'boo', true);
 
+function isBoo (state) {
+    return !(state % 0);
+}
+
 boo.registerEvent('up', function() { this.posY -= 10; });
 boo.registerEvent('down', function() { this.posY += 10; });
-boo.registerEvent('left', function() { this.posX -= 10; });
-boo.registerEvent('right', function() { this.posX += 10; });
-boo.registerEvent('boo!', function() { console.log('space!', this.sprite); this.sprite.changeState(1); });
+boo.registerEvent('left', function() {
+    if (this.currentState < 2) {
+        this.currentState += 2;
+    };
+    this.posX -= 10;
+});
+boo.registerEvent('right', function() { 
+    if(this.currentState >= 2){
+        this.currentState -= 2;
+    }
+    this.posX += 10;
+});
+boo.registerEvent('boo!', function() {
+    
+    if (isBoo(this.currentState)){
+        this.currentState--;
+    }
+    else{
+        this.currentState++;
+    }
+    
+});
 
 server.addObjects(boo);
 server.addConfiguration({players: 1});
+
 
 server.init();
