7f763801dfef472199fe4e1dbac928f8079d4573
Filipe La Ruina <filaruina@gmail.com>
2011-09-27 00:36:26 -0300

    Removing stepBack from wall since it is too buggy and adding score on the pong demo

--- Shared/Redread.js
+++ Shared/Redread.js
@@ -163,38 +163,39 @@
             };
 
             //used for when the step goes after the wall
+            /* TODO: Make it better, still quite buggy
             var stepBack = {
                 top: 0,
                 bottom: 0,
                 left: 0,
                 right: 0
-            };
+            };*/
 
             if (obj.posY <= boundaries.top) {
                 walls.top = true;
-                stepBack.top = obj.posY - boundaries.top;
+                //stepBack.top = obj.posY - boundaries.top;
             }
 
             if (obj.posY + height >= boundaries.bottom) {
                 walls.bottom = true;
-                stepBack.bottom = (obj.posY + height) - boundaries.bottom;
+                //stepBack.bottom = (obj.posY + height) - boundaries.bottom;
             }
 
             if (obj.posX <= boundaries.left) {
                 walls.left = true;
-                stepBack.left = obj.posX - boundaries.left;
+                //stepBack.left = obj.posX - boundaries.left;
             }
 
             if (obj.posX + width >= boundaries.right) {
                 walls.right = true;
-                stepBack.right = (obj.posX + width) - boundaries.right;
+                //stepBack.right = (obj.posX + width) - boundaries.right;
             }
 
             this.send(JSON.stringify({
                 type: 'wallHit',
                 id: obj.id,
-                walls: walls,
-                stepBack: stepBack
+                walls: walls//,
+                //stepBack: stepBack
             }));
         },
 
